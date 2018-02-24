c5ccbe3e90a3701273e1d538084ff3653a1c5373
JonyFang <jony.chunfang@gmail.com>
2018-02-24 16:06:33 +0800

    Init Project

---
+++ FFRainbowLabel/Sources/FFRainbowLabel.swift
@@ -0,0 +1,14 @@
+//
+//  FFRainbowLabel.swift
+//  FFRainbowLabel
+//
+//  Created by JonyFang on 2018/2/24.
+//  Copyright © 2018年 JonyFang. All rights reserved.
+//
+
+import UIKit
+
+public enum FFillOption {
+    case Background
+    case Text
+}
---
+++ FFRainbowLabel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FFRainbowLabel
+//
+//  Created by JonyFang on 2018/2/24.
+//  Copyright © 2018年 JonyFang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
